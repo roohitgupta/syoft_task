4aced36dc10f0dcee3ba5feb51aa5a05e0f71371
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-08-02 14:00:11 +0530

    build registration page along with fetch function

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function registerUser() {
+    fetch('http://localhost:3005/api/users/register', {})
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Register</h1>
+     <form action="" onSubmit={registerUser}>
+        <input 
+            type="text" 
+            placeholder="Name" 
+            value={name}
+            onChange={(e)=> setName(e.target.value)}
+        /> 
+        <br />
+        <input 
+            type="email" 
+            placeholder="email" 
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
+        />
+        <br />
+        <input 
+            type="password" 
+            placeholder="password" 
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+        />
+        <br />
+        <input type="submit" value='Register' />
+     </form>
     </div>
   );
 }
